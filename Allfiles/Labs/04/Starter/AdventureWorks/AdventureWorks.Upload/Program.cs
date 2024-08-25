2d777b1a3c7749ae17c0afefa25be6a828159a5c
shafiqul <shafiqul@DESKTOP-6TU3L2H>
2024-08-25 21:04:42 +0800

    Retrieve azure Cosmos DB collection by using the .NET SDK

--- Allfiles/Labs/04/Starter/AdventureWorks/AdventureWorks.Upload/Program.cs
+++ Allfiles/Labs/04/Starter/AdventureWorks/AdventureWorks.Upload/Program.cs
@@ -11,12 +11,12 @@
 
     public class Program
     {
-        private const string EndpointUrl = "";
-        private const string AuthorizationKey = "";
+        private const string EndpointUrl = "cosmob db url";
+        private const string AuthorizationKey = "cosmos db primary key";
         private const string DatabaseName = "Retail";
         private const string ContainerName = "Online";
-        private const string PartitionKey = "";
-        private const string JsonFilePath = "";
+        private const string PartitionKey = "/Category";
+        private const string JsonFilePath = "C:\\Users\\shafiqul\\source\\repos\\AZ-204\\DevelopingSolutionsforMicrosoftAzure\\Allfiles\\Labs\\04\\Starter\\AdventureWorks\\AdventureWorks.Upload\\models.json";
 
         static private int amountToInsert;
         static List<Model> models;
