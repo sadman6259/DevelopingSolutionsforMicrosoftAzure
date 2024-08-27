13a1e5a7d780235aa1381ecde34e442326368d64
shafiqul <shafiqul@DESKTOP-6TU3L2H>
2024-08-28 01:46:06 +0800

    Access resource secrets more securely across services using key vault

---
+++ Allfiles/Labs/07/Starter/func/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+             // If using Kestrel:
+    services.Configure<KestrelServerOptions>(options =>
+    {
+        options.AllowSynchronousIO = true;
+    });
+
+    // If using IIS:
+    services.Configure<IISServerOptions>(options =>
+    {
+        options.AllowSynchronousIO = true;
+    });
+    })
+    .Build();
+
+host.Run();
